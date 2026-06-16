c7aadd862a6fffc5eae57e0e44fc4c2bd9bfde90
Muskan377750 <nov6947sk@gmail.com>
2026-06-16 07:01:16 +0530

    program2.c

---
+++ Assignment_1/program2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x = 123;
+    float y = 34.67;
+    char z = 'A';
+    printf("%d\n",x);
+    printf("%.2f\n",y);
+    printf("%c\n",z);
+    return 0;
+}

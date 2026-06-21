000603b210ef9665b60b4f7870c9d0328385bf7c
Muskan377750 <nov6947sk@gmail.com>
2026-06-21 23:20:13 +0530

    program 3

---
+++ Assignment_4/program3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char c;
+    printf("Enter any character: ");
+    scanf(" %c",&c);
+    if(c>='a'&&c<='z'||c>='A'&&c<='Z'){
+        printf("Alphabet");
+    }else if(c>='0'&&c<='9'){
+        printf("Digit");
+    }else{
+        printf("Special Character");
+    }
+    return 0;
+}

3cb2457407286e1a5eb318f015a184e7bf339de6
Muskan377750 <nov6947sk@gmail.com>
2026-07-02 09:20:14 +0530

    Call by reference

---
+++ Day_12/callByReference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+};
+
+int main(){
+    int a = 200;
+    int b = 300;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    swap(a,b);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}

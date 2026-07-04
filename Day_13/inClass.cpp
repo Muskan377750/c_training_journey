5348c3252e7147ea6b081f8b280563eea93415da
Muskan377750 <nov6947sk@gmail.com>
2026-07-04 18:10:50 +0530

    Class

---
+++ Day_13/inClass.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class A{
+    public:
+    int a;
+    int b;
+    A(){}
+    A(int a, int b){
+        this->a = a;
+        this->b = b;
+    }
+    void show(){
+        cout<<a<<" "<<b<<endl;
+    };
+    void copy(A &ob){
+        a = ob.a;
+        b = ob.b;
+    };
+    static void add(A *ptr, A &ob2){
+    int sum1 = ptr->a+ob2.a;
+    int sum2 = ptr->b+ob2.b;
+    cout<<sum1<<" "<<sum2<<endl;
+};
+};
+
+int main(){
+    A obj1(34,56);
+    A obj2;
+    obj2.copy(obj1);
+    A::add(&obj1,obj2); // we can also write obj1.Add(obj2);
+    return 0;
+}

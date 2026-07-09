c86ce67e1650c395f05a99e5e5dfb30f60d9febd
Muskan377750 <nov6947sk@gmail.com>
2026-07-09 11:57:32 +0530

    Destructor

---
+++ Day_14/destructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class A{
+    int num;
+    public:
+    A(){
+        cout<<"A class constructor\n";
+    }
+    void show(){
+        cout<<num<<endl;
+    }
+};
+class B: public A{
+    public:
+    B(){
+        cout<<"B class constructor\n";
+    }
+};
+class C:public B{
+    public:
+    C(){
+        cout<<"C class constructor\n";
+    }
+    ~C(){
+        cout<<"C class Destructor\n";
+    }
+};
+int main(){
+    C obj;
+    obj.show();
+    cout<<"Happy Learning!!\n";
+    return 0;
+}
